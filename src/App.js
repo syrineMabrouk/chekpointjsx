fd20427b28a178295218eaaf9316bf4baeb8be2f
syrineMabrouk <sirine.mabrouk2016@gmail.com>
2021-05-11 11:45:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import myimg from "./images/JSX-vs-HTML.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 class="title red">syrine mabrouk </h1>
+
+        <br />
+        <img src={myimg} alt="jsx 2" />
+
+
+        <br />
+        <img src="./jsx.png" alt="jsx" />
+
+      </div>
+
+      <iframe width="800" height="315" src="https://www.youtube.com/embed/vf5KrZR-Vv4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     
     </div>
   );
 }
